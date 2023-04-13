8d82f61d87732bc5976138c00ba051f99d641919
Mithrilhall <eric_estes@aspensquare.com>
2023-04-13 10:12:02 -0400

    Adjust 404 error page.

--- app/[...catchAll]/page.js
+++ app/[...catchAll]/page.js
@@ -4,7 +4,10 @@ const CatchAll = () => {
     return (
         <main className={styles.main} style={{ margin: "auto" }}>
             <div className={styles.description} style={{  }}>
-                404
+                <h1 className="next-error-h1" style={{ display: "inline-block", margin: "0 20px 0 0", paddingRight: "23px", fontSize: "24px", fontWeight: "500", verticalAlign: "top", lineHeight: "49px" }}>404</h1>
+                <div style={{ display: "inline-block", textAlign: "left" }}>
+                    <h2 style={{ fontSize: "14px", fontWeight: "400", lineHeight: "49px", margin: "0"}}>This page could not be found</h2>
+                </div>
             </div>
         </main>
     );
