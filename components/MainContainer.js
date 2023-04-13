feb4be23b45346ebca8816579070415917495628
Mithrilhall <eric_estes@aspensquare.com>
2023-04-13 09:25:38 -0400

    Formatting

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -2,12 +2,12 @@
 
 import Container from "@mui/material/Container";
 
-const MainContainer = ({ children }) => {
-  return (
-    <Container maxWidth="xl">
-      {children}
-    </Container>
-  );
+const MainContainer = ( { children } ) => {
+    return (
+        <Container maxWidth="xl">
+            {children}
+        </Container>
+    );
 };
 
 export default MainContainer;
